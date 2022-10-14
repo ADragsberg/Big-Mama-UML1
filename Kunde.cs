8bf3c1e8c0710771284f11576a4eec9044c061b9
Andreas Dragsberg <andr31j3@edu.zealand.dk>
2022-10-14 12:17:53 +0200

    Kunde klassen færdig

--- Kunde.cs
+++ Kunde.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Kunde
+    public class Kunde
     {
+
+
+        #region Instance field
+        private string _firstname;
+        private string _lastname;
+
+        #endregion
+
+        #region Constructor
+        public Kunde(string firstname, string lastname)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+        }
+
+        #endregion
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstname ; }
+        }
+        public string LastName
+        {
+            get { return _lastname ; }
+        }
+        public string FullName
+        {
+            get { return $"{ _firstname} { _lastname}"; }
+        }
+
+        #endregion
+        #region Method
+        public override string ToString()
+        {
+            return $"Kunde: {FullName}";
+        }
+
+        #endregion
     }
 }
